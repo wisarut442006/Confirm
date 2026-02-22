7c5514833b50d6a2ab2f9d66486bff23f60374d2
wisarut442006 <wisarut442006@gmail.com>
2026-02-22 23:19:49 +0700

    d

---
+++ index.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<h1>Hello Web Programming Technology</h1>')
+  res.end('Hello\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+}); 
+
+
